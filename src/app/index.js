4d64b5e73ebfbb4d42f5f359ddf1bd936e6c893d
debug-Ankit <ankit.developer396@gmail.com>
2023-06-03 12:00:00 +0530

    Create APIs for booking details and reviews

--- src/app/index.js
+++ src/app/index.js
@@ -27,6 +27,8 @@ const authRoute = require("../routes/auth.routes");
 const userRoute = require('../routes/user.routes');
 const appsRoute = require('../routes/apps.routes');
 const roomRoute = require('../routes/room.routes');
+const bookingRoute = require('../routes/booking.route');
+const reviewRoute = require('../routes/review.routes');
 
 // load environment variables from .env file
 env.config();
@@ -82,6 +84,8 @@ app.use("/api/v1", authRoute); // auth routes
 app.use('/api/v1', userRoute); // user routes
 app.use('/api/v1', appsRoute); // apps routes
 app.use('/api/v1', roomRoute); // room routes
+app.use('/api/v1', bookingRoute); // booking routes
+app.use('/api/v1', reviewRoute); // review routes
 
 // 404 ~ not found error handler
 app.use(notFoundRoute);
